1910a5e5a31469931c26af9b12106e970a33d894
tejeshraovemula <143709843+tejeshraovemula@users.noreply.github.com>
2024-09-05 15:12:00 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -97,7 +97,7 @@ exports.execute = function (req, res) {
     var requestBody = req.body.inArguments[0];
 
    
-    const to = requestBody.code+requestBody.to;
+    const to = "+"+requestBody.to;
     const from = process.env.SENDER_PHONE;
     const body = requestBody.body;
     const contactKey = requestBody.contactKey;
