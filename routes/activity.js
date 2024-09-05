09c6d4acc04ba6e3480b13c1b31d119194955660
tejeshraovemula <143709843+tejeshraovemula@users.noreply.github.com>
2024-09-05 14:42:25 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -91,12 +91,8 @@ exports.save = function (req, res) {
  */
 exports.execute = function (req, res) {
 
-    console.log("5 -- For Execute");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Executed: "+req.body.inArguments[0]);
+   
+    console.log("Executed: "+req.body.inArguments[0]);
     
     var requestBody = req.body.inArguments[0];
 
