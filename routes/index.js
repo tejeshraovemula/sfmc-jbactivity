38d774c02315ef6f83c84e663cbfaaf94922fadd
tejeshraovemula <143709843+tejeshraovemula@users.noreply.github.com>
2024-09-12 10:12:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var activity = require('./activity');
  * GET home page.
  */
 exports.index = function(req, res){
-    
+    console.log("request :"+req);
     if( !req.session.token ) {
         console.log("Rendering Index Page1"); 
         res.render( 'index', {
